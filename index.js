34dbe23007b0e777688599f99443806f57744056
github username <github email address>
2019-09-16 19:45:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// MULTIPY NUM1*NUM2 = 62
+
+const num1 = 62 / 2
+const num2 = 62 / num1
+
+const multiply = num1 * num2
+
+
+// RANDOM RANDOM POSITIVE INTEGER
+const r = Math.random()
+const random = Math.ceil(r * 10)
+
+// MOD REMAINDER 4
+const mod = 10 % 6
+
+// MAX OF 20
+const max = Math.max(1, 5, 10, 15, 20)
